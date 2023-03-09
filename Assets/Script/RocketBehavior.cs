be657fe5a4c09d790fd066415092e4e020f4a9b5
ArDai <78299618+sean920310@users.noreply.github.com>
2023-03-10 02:13:10 +0800

    modify rocket rotation (not done)

--- Assets/Script/RocketBehavior.cs
+++ Assets/Script/RocketBehavior.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class RocketBehavior : MonoBehaviour
 {
+    public GameObject explotionPrefab;
+
+    private Rigidbody rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        gameObject.transform.rotation = Quaternion.Euler(rigidbody.velocity.normalized);
+    }
+
+    private void OnCollisionEnter(Collision collision)
     {
         
     }
