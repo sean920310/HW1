e54ed8ec4d434ff12f0d02c9678951f2c6ed4fdf
ArDai <78299618+sean920310@users.noreply.github.com>
2023-03-11 00:54:11 +0800

    Revert "revert to merge before"
    This reverts commit f495320cea1e5ff51683adcbddce3f29b21e8a25.

--- Assets/Script/TankControl.cs
+++ Assets/Script/TankControl.cs
@@ -4,109 +4,34 @@ using UnityEngine;
 
 public class TankControl : MonoBehaviour
 {
-    public Transform[] rightWheelMeshs;
-    public Transform[] leftWheelMeshs;
-    public WheelCollider[] rightWheelColliders;
-    public WheelCollider[] leftWheelColliders;
+    float verticalAxis, HorizontalAxis;
+
+    TankManager tankManager;
 
     public Transform cam;
-    public Transform tower;
-    public Transform canon;
-    public Transform aimTo;
-
-    public GameObject rocketPrefab;
-
-    public float Force, RotSpeed, breakForce, towerRotationSpeed;
-
-
-    Vector3 pos;
-    Quaternion quat;
-    float rightPower, leftPower, engineBreakForce;
-    float towerRotate, canonRotate;
-    GameObject prefab;
 
     // Start is called before the first frame update
     void Start()
     {
-        engineBreakForce = breakForce / 2f;
+        tankManager = transform.GetComponent<TankManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
         //wheelCollider move
-        rightPower = Input.GetAxis("Vertical");
-        leftPower = Input.GetAxis("Vertical");
+        verticalAxis = Input.GetAxis("Vertical");
+        HorizontalAxis = Input.GetAxis("Horizontal");
 
-        rightPower -= Input.GetAxis("Horizontal") * RotSpeed;
-        leftPower += Input.GetAxis("Horizontal") * RotSpeed;
-
-        for(int i=0;i<rightWheelMeshs.Length;i++)
-        {
-            rightWheelColliders[i].GetWorldPose(out pos, out quat);
-            rightWheelMeshs[i].position = pos;
-            rightWheelMeshs[i].rotation = quat;
-        }
-        for (int i = 0; i < leftWheelMeshs.Length; i++)
-        {
-            leftWheelColliders[i].GetWorldPose(out pos, out quat);
-            leftWheelMeshs[i].position = pos;
-            leftWheelMeshs[i].rotation = quat;
-        }
-
-        foreach (var wheelCols in rightWheelColliders)
-        {
-            wheelCols.motorTorque = rightPower * Force * Time.deltaTime;
-            if (Input.GetKey(KeyCode.Space))
-                wheelCols.brakeTorque = breakForce;
-            else if (rightPower == 0f)
-                wheelCols.brakeTorque = engineBreakForce;
-            else
-                wheelCols.brakeTorque = 0f;
-        }
-
-        foreach (var wheelCols in leftWheelColliders)
-        {
-            wheelCols.motorTorque = leftPower * Force * Time.deltaTime;
-            if (Input.GetKey(KeyCode.Space))
-                wheelCols.brakeTorque = breakForce;
-            else if (rightPower == 0f)
-                wheelCols.brakeTorque = engineBreakForce;
-            else
-                wheelCols.brakeTorque = 0f;
-        }
-
+        tankManager.Move(verticalAxis, HorizontalAxis);
 
         //tower & canon rotation
-        towerRotate = cam.eulerAngles.y - tower.eulerAngles.y;
-        canonRotate = cam.eulerAngles.x;
-        
-        if (towerRotate > 180f) towerRotate -= 360f; 
-        if (towerRotate < -180f) towerRotate += 360f;
-        if(towerRotate > 0.5f)
-        {
-            tower.Rotate(new Vector3(0f, towerRotationSpeed, 0f));
-        }
-        else if(towerRotate < -0.5f)
-        {
-            tower.Rotate(new Vector3(0f, -towerRotationSpeed, 0f));
-        }
-        canon.rotation = Quaternion.Euler(canonRotate, tower.eulerAngles.y, transform.rotation.eulerAngles.z);
+        tankManager.TowerAndCanonRotation(cam.eulerAngles);
 
         //fire
         if (Input.GetMouseButtonDown(0))
         {
-            Fire();
+            tankManager.Fire();
         }
-    }
-
-    void Fire()
-    {
-        Vector3 aimDir = aimTo.position - canon.position;
-        prefab = Instantiate(rocketPrefab, aimTo.position, aimTo.rotation);
-        prefab.GetComponent<Rigidbody>().AddForce(aimDir * 15000f);
-        Destroy(prefab, 1);//temp
-
-
     }
 }
