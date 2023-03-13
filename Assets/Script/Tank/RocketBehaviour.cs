6a23b0ba76a448f240568f5fa4778de8895e8efd
ArDai <78299618+sean920310@users.noreply.github.com>
2023-03-14 01:53:36 +0800

    bug fix

--- Assets/Script/Tank/RocketBehaviour.cs
+++ Assets/Script/Tank/RocketBehaviour.cs
@@ -36,7 +36,7 @@ public class RocketBehaviour : MonoBehaviour
 
     private void rocketDestory()
     {
-        explosion.Play();
+        explosionAudio.Play();
         GetComponent<Rigidbody>().velocity = Vector3.zero;
         GetComponent<MeshRenderer>().enabled = false;
         GetComponent<MeshCollider>().enabled = false;
